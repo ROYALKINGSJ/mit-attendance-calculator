77d3aecc5ff582ada540d3d13da78f5880d1b776
ROYALKINGSJ <4667590+ROYALKINGSJ@users.noreply.github.com>
2026-02-14 21:47:40 +0530

    added java week 5 codes

---
+++ JAVA_CODES/WEEK_5/WEEK_5_1.java
@@ -0,0 +1,56 @@
+package OOPS_LAB.WEEK_5;
+import java.util.Scanner;
+
+class STUDENT{
+    SUBJECT Subjects_object_array[];
+
+    void add_subjects(int sub_obj_number,int i){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter Subject Name : ");
+        String sub_name=sc.nextLine();
+
+        System.out.println("Enter Subject Marks : ");
+        int marks=sc.nextInt();
+
+        SUBJECT subject_object_in_add_student=new SUBJECT();
+
+        subject_object_in_add_student.assign(sub_name, marks);
+
+        Subjects_object_array[i]=subject_object_in_add_student;
+
+        subject_object_in_add_student.display();
+    }
+
+    class SUBJECT{
+        String subject_name;
+        int marks;
+
+        void assign(String subject_name, int marks){
+            this.subject_name=subject_name;
+            this.marks=marks;
+        }
+
+        void display(){
+            System.out.println("Subject Name : "+this.subject_name);
+            System.out.println("Marks : "+marks);
+        }
+    }
+}
+
+public class WEEK_5_1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Number Of Subjects : ");
+        int sub_obj_number=sc.nextInt();
+
+        STUDENT objector=new STUDENT();
+
+        objector.Subjects_object_array=new STUDENT.SUBJECT[sub_obj_number];
+
+        for (int i=0;i<sub_obj_number;i++){
+            objector.add_subjects(sub_obj_number,i);
+        }
+    }
+}
---
+++ JAVA_CODES/WEEK_5/WEEK_5_2.java
@@ -0,0 +1,81 @@
+package OOPS_LAB.WEEK_5;
+
+import java.util.Scanner;
+
+class EMPLOYEE{
+    String Employee_Name;
+    double Employee_Salary;
+    Department[] Dept_object;
+
+    class Department{
+        String Dept_Name;
+        String Dept_Location;
+
+        void set(String Dept_Name,String Dept_Location){
+            this.Dept_Name=Dept_Name;
+            this.Dept_Location=Dept_Location;
+        }
+
+        void display(){
+            System.out.println("Department Name : "+this.Dept_Name);
+            System.out.println("Department Location : "+this.Dept_Location);
+        }
+    }
+
+    void add_deparment(){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Number Of Departments : ");
+        int Number_Of_Departments=sc.nextInt();
+
+        Dept_object=new Department[Number_Of_Departments];
+
+        for (int i=0;i<Number_Of_Departments;i++){
+            Dept_object[i]=new Department();
+
+            System.out.println("Enter Department Name : ");
+            String Dept_Name=sc.nextLine();
+
+            System.out.println("Enter Department Location : ");
+            String Dept_Location=sc.nextLine();
+
+            Dept_object[i].set(Dept_Name, Dept_Location);
+        }
+    }
+
+    void display_all_details(){
+        System.out.println("Employee Name : "+Employee_Name);
+        System.out.println("Employee Salary : "+Employee_Salary);
+        for (int i=0;i<Dept_object.length;i++){
+            Dept_object[i].display();
+        }
+    }
+}
+
+public class WEEK_5_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Number Of Employees : ");
+        int Number_Of_Employees=sc.nextInt();
+
+        EMPLOYEE[] Employee_object_array=new EMPLOYEE[Number_Of_Employees];
+
+        for (int i=0;i<Number_Of_Employees;i++){
+            Employee_object_array[i]=new EMPLOYEE();
+
+            System.out.println("Enter Employee Name : ");
+            Employee_object_array[i].Employee_Name=sc.nextLine();
+
+            System.out.println("Enter Employee Salary : ");
+            Employee_object_array[i].Employee_Salary=sc.nextDouble();
+
+            Employee_object_array[i].add_deparment();
+
+            System.out.println("***********************************");
+
+            Employee_object_array[i].display_all_details();
+        }
+    }
+}
